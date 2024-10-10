339bdf0df81d1441659673663ef21146febb76b2
shahnazj <shahnaz.jafari35@gmail.com>
2024-10-10 22:54:55 +0200

    more practice of selector

--- script.js
+++ script.js
@@ -5,14 +5,47 @@
 
 const btn = document.querySelector('#btn')
 
-const para = document.querySelector('.para')
+const square = document.querySelector('.square')
+
+const ball= document.querySelector('.para')
+
 const pList = document.querySelectorAll('.para')
-const pList2 = document.querySelectorAll('.para')
 
-console.log(pList)
-console.log(pList2)
 
-pList.forEach(node => {
-    node.style.backgroundColor = "red"
+
+// square.addEventListener('mouseover', => {
+// square.classList.remove('bg-red')
+
+    
+// })
+// square.addEventListener('mouseover', => {
+// square.classList.remove('bg-red')
+    
+    
+// })
+
+square.addEventListener('click', (event) => {
+    //console.log(event)
+    //square.classList.toggle('bg-red')
+    console.log('square')
 })
 
+ball.addEventListener('click',( => {
+    e.stopPropagtion()
+console.log('clicked the ball')
+})
+
+const form = document.querySelector('#form');
+
+form.addEventListener('submit',(e) =>{
+    e.preventDefault()
+    console.log('submitted')
+})
+
+Document.querySelector('#link').addEventListener('click',(e) => {
+    e.preventDefault*
+
+    // console.log('hahah jag trode du va')
+    // window.location.herf = 'https://www.aftonbladet.se'
+
+})
